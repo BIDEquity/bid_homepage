3104effe41f6f8474dad373b22e8dacfbee80494
Meinolf Droste <mdwp.post@gmail.com>
2021-04-14 12:03:16 +0200

    design fixes

--- src/Widgets/CardContainerWidget/CardContainerWidgetComponent.js
+++ src/Widgets/CardContainerWidget/CardContainerWidgetComponent.js
@@ -8,8 +8,8 @@ Scrivito.provideComponent('CardContainerWidget', ({ widget }) => (
     attribute="content"
     className="posts_wrap"
     />
-    <div class="posts_more">
-				<a class="more_link" href="#">More news</a>
+    <div className="posts_more">
+				<a className="more_link" href="#">More news</a>
 			</div>
   </div>
 ))
