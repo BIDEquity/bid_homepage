ffc7d07bdc56f1b95d3cbb26581e63c6f89fb4f4
Meinolf Droste <mdwp.post@gmail.com>
2021-04-29 11:26:56 +0200

    debug

--- src/Components/Navigation/LanguageSwitch.js
+++ src/Components/Navigation/LanguageSwitch.js
@@ -1,9 +1,8 @@
-import * as React from 'react';
-import * as Scrivito from 'scrivito';
-import getRoot from '../../utils/getRoot';
+import * as React from "react";
+import * as Scrivito from "scrivito";
+import getRoot from "../../utils/getRoot";
 
 class LanguageSwitch extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -11,54 +10,57 @@ class LanguageSwitch extends React.Component {
     this.state = {
       language: false,
     };
-
   }
 
   toggleLanguage() {
-
     this.setState({
       language: !this.state.language,
-
-    }
-    )
-
-
+    });
   }
 
   render() {
-    const homepages = Scrivito.getClass('Homepage').all().toArray();
-    
-  
+    const homepages = Scrivito.Obj.where(
+      "_objClass",
+      "equals",
+      "Homepage"
+    ).toArray();
+
     return (
       <ul className="nav navbar-nav navbar-right lang-switch">
-        <li className={`nav-item ${this.state.language ? 'open' : ''}`}>
-          <a className="nav-link" onMouseEnter={this.toggleLanguage} onClick={this.toggleLanguage}>{
-            getRoot() === 'de' ? 'DE' : 'EN'
-          }<i className="fa fa-chevron-down" /></a>
+        <li className={`nav-item ${this.state.language ? "open" : ""}`}>
+          <a
+            className="nav-link"
+            onMouseEnter={this.toggleLanguage}
+            onClick={this.toggleLanguage}
+          >
+            {getRoot() === "de" ? "DE" : "EN"}
+            <i className="fa fa-chevron-down" />
+          </a>
 
-
-
-          <ul onMouseLeave={this.toggleLanguage} className={`dropdown-menu ${this.state.language ? 'show-language menu-toggle' : 'hide-language'}`}>
-            {homepages.map(homepage =>
+          <ul
+            onMouseLeave={this.toggleLanguage}
+            className={`dropdown-menu ${
+              this.state.language
+                ? "show-language menu-toggle"
+                : "hide-language"
+            }`}
+          >
+            {homepages.map((homepage) => (
               <li className="nav-item" key={homepage.id()}>
-                <Scrivito.LinkTag onClick={this.toggleLanguage} to={homepage} className="nav-link">
-                  
-                  {
-                    homepage.id() === '73135f17d58607f8' ? 'EN'
-                      :  'DE'
-                        
-                  }
+                <Scrivito.LinkTag
+                  onClick={this.toggleLanguage}
+                  to={homepage}
+                  className="nav-link"
+                >
+                  {homepage.id() === "73135f17d58607f8" ? "EN" : "DE"}
                 </Scrivito.LinkTag>
               </li>
-
-            )}
+            ))}
           </ul>
         </li>
-
       </ul>
     );
   }
 }
 
 export default Scrivito.connect(LanguageSwitch);
-
