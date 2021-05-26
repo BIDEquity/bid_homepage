31a75edb7da9dc4364f3d7c56f2978bec94de15d
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-05-26 12:37:32 +0200

    css fixes

--- src/Widgets/BidWhatWeDoRowWidget/BidWhatWeDoRowWidgetComponent.js
+++ src/Widgets/BidWhatWeDoRowWidget/BidWhatWeDoRowWidgetComponent.js
@@ -2,40 +2,37 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 
 Scrivito.provideComponent("BidWhatWeDoRowWidget", ({ widget }) => {
- return  (
-    <div className={`we_do_row ${widget.get("reverse") === "yes" && "reverse"}`}>
-    <div className="we_do_img">
-        <Scrivito.ImageTag
-            content={widget}
-            attribute="image"
-            alt=""
-        />
-    </div>
-    <div className="we_do_info">
-        
+  const link = widget.get("link");
+  let linktext = link && link.title();
+
+  return (
+    <div
+      className={`we_do_row ${widget.get("reverse") === "yes" && "reverse"}`}
+    >
+      <div className="we_do_img">
+        <Scrivito.ImageTag content={widget} attribute="image" alt="" />
+      </div>
+      <div className="we_do_info">
         <Scrivito.ContentTag
-            content={widget}
-            attribute="headline"
-            className="we_do_title"
-            tag="h2"
+          content={widget}
+          attribute="headline"
+          className="we_do_title"
+          tag="h2"
         />
-        
+
         <Scrivito.ContentTag
-            content={widget}
-            attribute="description"
-            className="we_do_description"
-            tag="p"
+          content={widget}
+          attribute="description"
+          className="we_do_description"
+          tag="p"
         />
         <div className="we_do_text">
-        <Scrivito.ContentTag
-            content={widget}
-            attribute="text"
-            tag="p"
-        />
+          <Scrivito.ContentTag content={widget} attribute="text" tag="p" />
         </div>
-        <a className="we_do_more more_link" href="#">Our investment criteria</a>
+        <Scrivito.LinkTag to={link} className="we_do_more more_link">
+            {linktext}
+        </Scrivito.LinkTag>
+      </div>
     </div>
-</div>
- )
-      
+  );
 });
