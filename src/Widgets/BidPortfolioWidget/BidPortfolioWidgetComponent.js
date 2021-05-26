313f25fe36503a627fab257de37fd03190cde887
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-05-26 11:29:35 +0200

    team

--- src/Widgets/BidPortfolioWidget/BidPortfolioWidgetComponent.js
+++ src/Widgets/BidPortfolioWidget/BidPortfolioWidgetComponent.js
@@ -4,72 +4,96 @@ import * as Scrivito from "scrivito";
 Scrivito.provideComponent("BidPortfolioWidget", ({ widget }) => {
   const link = widget.get("sectionLink");
   let linktext = link && link.title();
-  
 
   return (
     <>
-    <div className="portfolio_posts">
-		<div className="container">
-            <div className="portfolio_posts_wrap">
-                <div className="portfolio_post_item">
-                    <div className="portfolio_post_img">
-                        <img src="images/portfolio_post.jpg" alt=""/>
-                    </div>
-                    <div className="portfolio_post_info">
-                        <p className="portfolio_post_name bottom_line light_line">Company Name</p>
-                        <div className="portfolio_post_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ut.</p>
-                        </div>
-                    </div>
+      <div className="portfolio_posts">
+        <div className="container">
+          <div className="portfolio_posts_wrap">
+            <div className="portfolio_post_item">
+              <div className="portfolio_post_img">
+                <img src="images/portfolio_post.jpg" alt="" />
+              </div>
+              <div className="portfolio_post_info">
+                <p className="portfolio_post_name bottom_line light_line">
+                  Company Name
+                </p>
+                <div className="portfolio_post_text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna ut.
+                  </p>
                 </div>
-                <div className="portfolio_post_item">
-                    <div className="portfolio_post_logo">
-                        <img src="images/portfolio_logo_1.png" width="235" alt=""/>
-                    </div>
-                    <div className="portfolio_post_info">
-                        <p className="portfolio_post_name bottom_line light_line">Company Name</p>
-                        <div className="portfolio_post_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ut.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="portfolio_post_item">
-                    <div className="portfolio_post_img">
-                        <img src="images/portfolio_post.jpg" alt=""/>
-                    </div>
-                    <div className="portfolio_post_info">
-                        <p className="portfolio_post_name bottom_line light_line">Company Name</p>
-                        <div className="portfolio_post_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ut.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="portfolio_post_item">
-                    <div className="portfolio_post_logo">
-                        <img src="images/portfolio_logo_1.png" width="235" alt=""/>
-                    </div>
-                    <div className="portfolio_post_info">
-                        <p className="portfolio_post_name bottom_line light_line">Company Name</p>
-                        <div className="portfolio_post_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ut.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="portfolio_post_item">
-                    <div className="portfolio_post_img">
-                        <img src="images/portfolio_post.jpg" alt=""/>
-                    </div>
-                    <div className="portfolio_post_info">
-                        <p className="portfolio_post_name bottom_line light_line">Company Name</p>
-                        <div className="portfolio_post_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ut.</p>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div className="portfolio_post_item">
+              <div className="portfolio_post_logo">
+                <img src="images/portfolio_logo_1.png" width="235" alt="" />
+              </div>
+              <div className="portfolio_post_info">
+                <p className="portfolio_post_name bottom_line light_line">
+                  Company Name
+                </p>
+                <div className="portfolio_post_text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna ut.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="portfolio_post_item">
+              <div className="portfolio_post_img">
+                <img src="images/portfolio_post.jpg" alt="" />
+              </div>
+              <div className="portfolio_post_info">
+                <p className="portfolio_post_name bottom_line light_line">
+                  Company Name
+                </p>
+                <div className="portfolio_post_text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna ut.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="portfolio_post_item">
+              <div className="portfolio_post_logo">
+                <img src="images/portfolio_logo_1.png" width="235" alt="" />
+              </div>
+              <div className="portfolio_post_info">
+                <p className="portfolio_post_name bottom_line light_line">
+                  Company Name
+                </p>
+                <div className="portfolio_post_text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna ut.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="portfolio_post_item">
+              <div className="portfolio_post_img">
+                <img src="images/portfolio_post.jpg" alt="" />
+              </div>
+              <div className="portfolio_post_info">
+                <p className="portfolio_post_name bottom_line light_line">
+                  Company Name
+                </p>
+                <div className="portfolio_post_text">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna ut.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    <div className="info_section">
+      </div>
+      <div className="about_us">
         <Scrivito.ImageTag
           content={widget}
           attribute="bgImage"
@@ -77,48 +101,40 @@ Scrivito.provideComponent("BidPortfolioWidget", ({ widget }) => {
           alt=""
         />
         <div className="info_slider swiper-container">
-            <div className="swiper-wrapper">
-                <div className="info_slider_item swiper-slide">
-                    <div className="container">
-                        <Scrivito.ContentTag
-                          content={widget}
-                          attribute="sectionHeadline"
-                          className="section_title bottom_line col_6 col_t_12"
-                          tag="h2"
-                        />
-                        
-                        <Scrivito.LinkTag to={link} className="info_slider_link">
-                          {linktext}
-                        </Scrivito.LinkTag>
-                    </div>
-                </div>
-                
-        </div>
-        <div className="info_slider_bottom">
-            <div className="container">
-                <div className="info_slider_pagination slider_pagination"></div>
+          <div className="swiper-wrapper">
+            <div className="info_slider_item swiper-slide">
+              <div className="container">
+                <Scrivito.ContentTag
+                  content={widget}
+                  attribute="sectionHeadline"
+                  className="section_title bottom_line col_6 col_t_12"
+                  tag="h2"
+                />
+
+                <Scrivito.LinkTag to={link} className="info_slider_link">
+                  {linktext}
+                </Scrivito.LinkTag>
+              </div>
             </div>
+          </div>
+          <div className="info_slider_bottom">
+            <div className="container">
+              <div className="info_slider_pagination slider_pagination"></div>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
-    <div className="portfolio_news">
-		<div className="container">
-			
-      <Scrivito.ContentTag
-        content={widget}
-        attribute="newsHeadline"
-        tag="h2"
-        className="section_title bottom_line"
-      />
-      <Scrivito.ContentTag
-        content={widget}
-        attribute="content"
-      />
-		</div>
-			</div>
-			
-	
-	
-  </>
+      </div>
+      <div className="portfolio_news">
+        <div className="container">
+          <Scrivito.ContentTag
+            content={widget}
+            attribute="newsHeadline"
+            tag="h2"
+            className="section_title bottom_line"
+          />
+          <Scrivito.ContentTag content={widget} attribute="content" />
+        </div>
+      </div>
+    </>
   );
 });
