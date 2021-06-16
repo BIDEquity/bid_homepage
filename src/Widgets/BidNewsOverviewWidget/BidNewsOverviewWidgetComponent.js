825448576f307f6dd7cdaa34493f6bf073990512
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-06-16 17:18:45 +0200

    news select box

--- src/Widgets/BidNewsOverviewWidget/BidNewsOverviewWidgetComponent.js
+++ src/Widgets/BidNewsOverviewWidget/BidNewsOverviewWidgetComponent.js
@@ -3,8 +3,14 @@ import * as Scrivito from "scrivito";
 
 import formatDate from "../../utils/formatDate";
 import InPlaceEditingPlaceholder from "../../Components/InPlaceEditingPlaceholder";
-import TagList from "../../Components/TagList";
-import "./BidNewsOverviewWidget.scss";
+import Select from "react-select";
+
+const companies = [
+  { value: "evidentic", label: "Evidentic" },
+  { value: "radical-path", label: "Radical Path" },
+  { value: "just-relate", label: "Just Relate" },
+  { value: "microsoft", label: "Microsoft" },
+];
 
 class BidNewsOverviewWidgetComponent extends React.Component {
   constructor(props) {
@@ -28,62 +34,143 @@ class BidNewsOverviewWidgetComponent extends React.Component {
     }
 
     return (
-      <div class="news_content">
-		<div class="container">
-    <div class="news_panel">
-                <nav class="news_categories bottom_line">
-                    <ul>
-                        <li className="active"><a href="#">All</a></li>
-                        <li><a href="#">Acquisitions</a></li>
-                        <li><a href="#">Exits</a></li>
-                        <li><a href="#">People</a></li>
-                        <li><a href="#">Events</a></li>
-                        <li><a href="#">Market News</a></li>
-                        <li><a href="#">Insights</a></li>
-                        <li><a href="#">Tools</a></li>
-                    </ul>
-                </nav>
-                <div className="news_sort">
-                    <select className="select_style" data-placeholder="Sort by Portfolio Company">
-                        <option></option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                        <option>EvidentIQ</option>
-                    </select>
-                </div>
-            </div>
-      <div className="posts_wrap">
-        {news.map((news, index) => {
-          console.log(index)
-          return (
-            
-              <a key={index} class="post_item" href="#">
-                <div className="post_item_img">
+      <div className="news_content">
+        <div className="container">
+          <div className="news_panel">
+            <nav className="news_categories bottom_line">
+              <ul>
+                <li className="active">
+                  <a href="#">All</a>
+                </li>
+                <li>
+                  <a href="#">Acquisitions</a>
+                </li>
+                <li>
+                  <a href="#">Exits</a>
+                </li>
+                <li>
+                  <a href="#">People</a>
+                </li>
+                <li>
+                  <a href="#">Events</a>
+                </li>
+                <li>
+                  <a href="#">Market News</a>
+                </li>
+                <li>
+                  <a href="#">Insights</a>
+                </li>
+                <li>
+                  <a href="#">Tools</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="news_sort">
+              
+              <Select
+                  //value={this.state.selectedLoc}
+                  //onChange={this.handleChangeLoc}
+                  options={companies}
+                  placeholder="Sort by Portfolio Company"
+                  className="select_style"
+                  classNamePrefix="companies-select"
                   
-                  <Scrivito.ImageTag
-                    content={news}
-                    attribute="newsImage"
-                    alt=""
-                  />
-                </div>
-                <div className="post_item_info">
-                  <p className="post_item_date bottom_line">
-                    {formatDate(news.get("date"), "dd mmm yyyy")}
-                  </p>
-                  <div className="post_item_text">
-                    <p>{news.get("teaser")}</p>
+               />
+            </div>
+          </div>
+          <div className="posts_wrap">
+            {news.map((news, index) => {
+              if (index === 8)
+                return (
+                  <div className="news_subscribe">
+                    <div className="news_subscribe_description">
+                      <h3 className="news_subscribe_title">
+                        Stay up to date with BID Equity
+                      </h3>
+                      <div className="news_subscribe_text">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua.
+                        </p>
+                      </div>
+                    </div>
+                    <form
+                      className="news_subscribe_form"
+                      action="#"
+                      method="POST"
+                    >
+                      <div className="news_subscribe_body">
+                        <div className="form_field">
+                          <input
+                            className="form_email news_subscribe_field subscribe_group"
+                            type="email"
+                            name="subscribe_email"
+                            placeholder="Email"
+                          />
+                        </div>
+                        <div className="request_errors"></div>
+                        <div className="news_subscribe_terms">
+                          <label className="subscribe_checkbox">
+                            <input
+                              className="subscribe_group subscribe_agree subscribe_terms"
+                              type="checkbox"
+                              name="subscribe_terms"
+                            />
+                            <span className="subscribe_checkbox_text">
+                              <a href="#">Terms and conditions</a> lorem ipsum
+                              dolor sit amet, consectetur adipiscing elit, sed
+                              do eiusmod tempor incididunt ut labore et dolore
+                              magna aliqua
+                            </span>
+                          </label>
+                          <label className="subscribe_checkbox">
+                            <input
+                              className="subscribe_group subscribe_agree subscribe_privacy"
+                              type="checkbox"
+                              name="subscribe_privacy"
+                            />
+                            <span className="subscribe_checkbox_text">
+                              <a href="#">Privacy Policy</a> lorem ipsum dolor
+                              sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut labore et dolore
+                              magna aliqua
+                            </span>
+                          </label>
+                        </div>
+                      </div>
+                      <div className="news_subscribe_bottom">
+                        <input
+                          className="news_subscribe_send btn"
+                          type="submit"
+                          value="Sign me up"
+                        />
+                      </div>
+                    </form>
                   </div>
-                </div>
-              </a>
-            
-          );
-        })}
-      </div>
-      </div>
+                );
+              return (
+                <a key={index} className="post_item" href="#">
+                  <div className="post_item_img">
+                    <Scrivito.ImageTag
+                      content={news}
+                      attribute="newsImage"
+                      alt=""
+                    />
+                  </div>
+                  <div className="post_item_info">
+                    <p className="post_item_date bottom_line">
+                      {formatDate(news.get("date"), "dd mmm yyyy")}
+                    </p>
+                    <div className="post_item_text">
+                      <p>{news.get("teaser")}</p>
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
