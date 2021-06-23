a8c7c6634691672d4f46137a5a0897fd05eeb733
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-06-23 16:58:19 +0200

    fix mobile nav

--- src/Components/Navigation/NavChild.js
+++ src/Components/Navigation/NavChild.js
@@ -120,7 +120,7 @@ function isActive(page) {
   const currentPath = Scrivito.currentPage().path();
   const obj = Scrivito.currentPage();
 
-  /*if(!page.permalink() || !obj.permalink()) {
+  if(!page.permalink() || !obj.permalink()) {
     return false;
   }
 
@@ -147,7 +147,7 @@ function isActive(page) {
   if (page.permalink().startsWith("team")) {
     return obj.permalink().startsWith("team");
     
-  }*/
+  }
 
   if (currentPath) {
     return currentPath.startsWith(page.path());
