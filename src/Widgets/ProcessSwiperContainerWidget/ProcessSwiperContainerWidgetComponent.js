367c71fc23f79a47f11f56c11b005090d5f3ed83
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-07-14 17:45:31 +0200

    process slider

--- src/Widgets/ProcessSwiperContainerWidget/ProcessSwiperContainerWidgetComponent.js
+++ src/Widgets/ProcessSwiperContainerWidget/ProcessSwiperContainerWidgetComponent.js
@@ -4,10 +4,10 @@ import InPlaceEditingPlaceholder from "../../Components/InPlaceEditingPlaceholde
 import ButtonTagList from "../../Components/ButtonTagList";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { EffectFade, Pagination } from "swiper";
+import SwiperCore, { Mousewheel, EffectFade, Pagination } from "swiper";
 
 // install Swiper modules
-SwiperCore.use([EffectFade, Pagination]);
+SwiperCore.use([Mousewheel, EffectFade, Pagination]);
 
 class ProcessSwiperComponent extends React.Component {
   constructor(props) {
@@ -18,15 +18,21 @@ class ProcessSwiperComponent extends React.Component {
     const widget = this.props.widget;
     const items = widget.get("items");
     const params = {
-      loop: "true",
+      loop: false,
       slidesPerView: 1,
-      speed: 500,
-      fadeEffect: { crossFade: true },
+      direction: "vertical",
+      speed: 2000,
+      pagination: {
+        el: ".slider_pagination",
+        clickable: true,
+      },
+      mousewheel: true,
+      allowTouchMove: true,
+      autoHeight: true,
       effect: "fade",
 
-      pagination: {
-        el: ".partners_slider_pagination",
-        clickable: "true",
+      fadeEffect: {
+        crossFade: true,
       },
     };
 
@@ -41,10 +47,68 @@ class ProcessSwiperComponent extends React.Component {
     return (
       <Swiper {...params}>
         {items.map((item, index) => {
+          const link = item.get("button");
+          const linktext = link && link.title();
           return (
             <SwiperSlide key={index}>
-              <div className="partners_wrap">
-                
+              
+              <div className="transaction_process_section">
+                <div className="slider_pagination transaction_process_dots"></div>
+
+                <div className="transaction_process_line line_1"></div>
+                <div className="transaction_process_line line_2"></div>
+                <div className="transaction_process_line line_3"></div>
+                <div className="transaction_process_line line_4"></div>
+                <div className="transaction_process_line line_5"></div>
+                <div className="transaction_process_slider swiper-container">
+                  <div className="swiper-wrapper">
+                    <div className="transaction_process_item swiper-slide">
+                    
+                      <div className={`transaction_process_content step_${index + 1}`}>
+                        <div className="transaction_process_left">
+                          <div className="transaction_process_box">
+                            <Scrivito.ContentTag
+                              content={item}
+                              attribute="stepTitle"
+                              tag="p"
+                              className="transaction_process_name"
+                            />
+                          </div>
+                        </div>
+                        <div className="transaction_process_center">
+                          <div className="transaction_process_block">
+                            <div className="transaction_process_text">
+                              <Scrivito.ContentTag
+                                content={item}
+                                attribute="content"
+                              />
+                            </div>
+                            
+                            {
+                              linktext &&
+                              <Scrivito.LinkTag className="btn" to={link}>{linktext}</Scrivito.LinkTag>
+                            }
+                          </div>
+                        </div>
+                        <div className="transaction_process_right">
+                          <div className="transaction_duration">
+                            {item.get("duration") &&
+                            <h3 className="transaction_duration_title">
+                              Duration
+                            </h3>
+                            }
+                            <Scrivito.ContentTag
+                              content={item}
+                              attribute="duration"
+                              tag="p"
+                              className="transaction_process_text"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </SwiperSlide>
           );
