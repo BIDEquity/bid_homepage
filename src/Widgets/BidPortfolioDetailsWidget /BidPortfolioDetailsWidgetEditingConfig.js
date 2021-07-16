401b3f9585675b997220767aaa8f04b75221a6d9
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-07-16 16:34:48 +0200

    portfolio detail page

--- src/Widgets/BidPortfolioDetailsWidget /BidPortfolioDetailsWidgetEditingConfig.js
+++
@@ -1,6 +0,0 @@
-import * as Scrivito from "scrivito";
-
-Scrivito.provideEditingConfig("BidPortfolioDetailsWidget", {
-  title: "Bid Portfolio Details",
-  properties: ['headline','bgImage','detailsButton']
-});
