7e3748fc6f330a08228ada9f16684c992e9f254d
Meinolf Droste <md@MacBook-Pro-von-Meinolf.local>
2021-08-20 16:21:49 +0200

    clean up

--- src/Widgets/GroupWidget/GroupWidgetEditingConfig.js
+++
@@ -1,7 +0,0 @@
-import * as Scrivito from "scrivito";
-import groupWidgetIcon from "../../assets/images/group_widget.svg";
-
-Scrivito.provideEditingConfig("GroupWidget", {
-  title: "Group",
-  thumbnail: groupWidgetIcon,
-});
