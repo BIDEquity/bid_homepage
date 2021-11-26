7feaf32283535f04ff6895cc18ece408be5d07d1
M. Droste <m.droste@mdwp.de>
2021-11-26 15:48:12 +0100

    mobile fixes

--- src/Components/CookieConsentBanner.js
+++ src/Components/CookieConsentBanner.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 import { useCookieConsent } from "./CookieConsentContext";
 import cookieConsentIcon from "../assets/images/cookie_consent_icon.svg";
+import { Button } from "reactstrap";
 
 function CookieConsentBanner() {
   const consentUrl = cookieConsentUrl();
@@ -15,8 +16,6 @@ function CookieConsentBanner() {
   React.useEffect(() => {
     setVisible(cookieConsentChoice === "undecided");
   }, [cookieConsentChoice]);
-
- 
 
   if (!visible || !consentUrl) {
     return null;
@@ -33,58 +32,73 @@ function CookieConsentBanner() {
 }
 
 function CookieBanner(props) {
-  
   return (
     <div className="popup_cookies">
       <div className="container">
         <div className="popup_cookies_inner">
-          <div className="popup_cookies_left">
-            <div className="popup_cookies_text">
-              <p>
-                Cookies help us deliver our services. By using our services, you
-                agree to our use of cookies.
-              </p>
-            </div>
+          
+        
+        <div className="popup_cookies_left">
+          
+          <div className="popup_cookies_text">
+            <p>
+            Our website uses cookies to analyze how the site is used and to
+            ensure your experience is consistent between visits.
+            </p>
           </div>
-          <div className="popup_cookies_right">
-            <a className="more_link" href={props.url} rel="noopener noreferrer">
+        </div>
+      
+      <div className="popup_cookies_right">
+      <a
+              className="more_link"
+              href={props.url}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               {props.title}
             </a>
-            <button className="btn btn-primary" onClick={props.onAccept}>
-              Accept
-            </button>
-          </div>
-          <button className="popup_cookies_close">
-            <div onClick={props.onDecline}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                color="red"
-                width="18.121"
-                height="18.121"
-                viewBox="0 0 18.121 18.121"
-                fill="none"
-                strokeWidth="1.5"
-              >
-                <g transform="translate(-478.939 -846.939)">
-                  <line
-                    x1="16"
-                    y2="16"
-                    transform="translate(480 848)"
-                    strokeLinecap="round"
-                  />
-                  <line
-                    x1="16"
-                    y1="16"
-                    transform="translate(480 848)"
-                    strokeLinecap="round"
-                  />
-                </g>
-              </svg>
-            </div>
-          </button>
+        <button
+          className="popup_cookies_accept button"
+          onClick={props.onAccept}
+        >
+          Accept
+        </button>
+        
+        </div>
+        <button className="popup_cookies_close"
+          
+          onClick={props.onDecline}
+        >
+          <svg
+              onClick={() => setPopup("")}
+              xmlns="http://www.w3.org/2000/svg"
+              width="18.121"
+              height="18.121"
+              viewBox="0 0 18.121 18.121"
+              fill="red"
+              stroke="red"
+              strokeWidth="1.5"
+            >
+              <g transform="translate(-478.939 -846.939)">
+                <line
+                  x1="16"
+                  y2="16"
+                  transform="translate(480 848)"
+                  strokeLinecap="round"
+                />
+                <line
+                  x1="16"
+                  y1="16"
+                  transform="translate(480 848)"
+                  strokeLinecap="round"
+                />
+              </g>
+            </svg>
+        </button>
         </div>
       </div>
     </div>
+    
   );
 }
 
@@ -96,7 +110,6 @@ function cookieConsentUrl() {
   }
 
   const cookieConsentLink = root.get("cookieConsentLink");
-  
 
   if (!cookieConsentLink) {
     return null;
@@ -104,7 +117,7 @@ function cookieConsentUrl() {
 
   return {
     url: Scrivito.urlFor(cookieConsentLink),
-    title: cookieConsentLink.title() || "Learn more",
+    title: cookieConsentLink.title() || "Learn more »",
   };
 }
 
