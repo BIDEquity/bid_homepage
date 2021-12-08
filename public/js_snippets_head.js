5cc55973790e3173006efbb07e72cc005eda78c2
M. Droste <m.droste@mdwp.de>
2021-12-08 13:01:25 +0100

    lfeeder

--- public/js_snippets_head.js
+++ public/js_snippets_head.js
@@ -3,3 +3,4 @@
 // Don't forget to whitelist the URLs in the CSP at public/_headersCsp.json.
 
 // Google Analytics and Intercom are already taken care of by the app, so there's no need to add them here.
+(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('4lZPGEjXoN3gLpBk');
