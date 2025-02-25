e024b18fb3e0af087d8e707a0e4205aea0992ea0
BID-Equity <91619247+BID-Equity@users.noreply.github.com>
2025-02-25 23:04:05 +0100

    Added Crisp component to App

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Tracking from "./Components/Tracking";
 import { CookieConsentProvider } from "./Components/CookieConsentContext";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import CrispChat from "./CrispChat";
+import { Crisp } from "crisp-sdk-web";
 
 
 
@@ -23,7 +23,7 @@ export const helmetContext = {};
 export default function App() {
   React.useEffect(() => {
     AOS.init();
-    
+    Crisp.configure("1c98242d-1098-4be6-a8ad-8b5c27d2fe0a");
   })
 
   function offsetTop(el) {
