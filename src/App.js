299806fb9d587e11f5b6454eaa67f078730488c0
BID-Equity <91619247+BID-Equity@users.noreply.github.com>
2025-02-25 23:17:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as Scrivito from "scrivito";
 import { HelmetProvider } from "react-helmet-async";
-import { Crisp } from "crisp-sdk-web";
 
 import CurrentPageMetadata from "./Components/CurrentPageMetadata";
 import ErrorBoundary from "./Components/ErrorBoundary";
@@ -14,20 +13,61 @@ import Tracking from "./Components/Tracking";
 import { CookieConsentProvider } from "./Components/CookieConsentContext";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Crisp } from "crisp-sdk-web";
 
 export const helmetContext = {};
 
 export default function App() {
   React.useEffect(() => {
     AOS.init();
+  })
 
-    // Initialize Crisp chat widget
-    // Crisp.configure("1c98242d-1098-4be6-a8ad-8b5c27d2fe0a");
-  }, []);
+  function offsetTop(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop }
+}
 
+  /* Step content animation */
+  const b = document.querySelector('body');
+  window.addEventListener('scroll', event => {
+    if (typeof b.dataset.scrollval === 'undefined') {
+      b.dataset.scrollval = 0;
+    }
+
+    let fromTop = window.scrollY;
+    let offset = 370;
+    
+    const steps = document.getElementsByClassName('content_step_section')
+   
+    for (let i = 0; i < steps.length; i++) {
+      let elOffset = offsetTop(steps[i]);
+      
+      if (elOffset.top - offset <= fromTop && elOffset.top - offset + steps[i].offsetHeight > fromTop) {
+        steps[i].classList.add('animate');
+        if (window.scrollY > b.dataset.scrollval) {
+          steps[i].classList.add('animation_reverse');
+        }
+        else if (window.scrollY < b.dataset.scrollval) {
+          steps[i].classList.remove('animation_reverse');
+        }
+      }
+      else {
+        steps[i].classList.remove('animate');
+        // setTimeout(function() {
+        //     section.classList.remove('animation_reverse');
+        // }, 2200)
+      }
+    }
+  
+
+    b.dataset.scrollval = window.scrollY;
+  })
+  
   return (
     <ErrorBoundary>
       <CookieConsentProvider>
+        <CrispChat />
         <HelmetProvider context={helmetContext}>
           <div>
             <div className="content-wrapper">
